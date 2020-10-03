05d8b7d1e5f589158d253eff9ea34c1ff587c7fd
Hu Chia-Wei <huchiawei@pthree.co.jp>
2020-10-03 14:57:15 +0900

    Impl superuser

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -29,3 +29,12 @@ class ModelTests(TestCase):
 
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user(None, 'test0000')
+    
+    def test_create_new_superuser(self):
+        """Test creating a new superuser"""
+        user = get_user_model().objects.create_superuser(
+            'admin@cw.pthree.com', 'admin0000'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
